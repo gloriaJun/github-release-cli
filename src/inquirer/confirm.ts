e8f5654688d06cef656918abec2111a6050a4ccc
gloriaJun <pureainu@gmail.com>
2020-11-18 18:20:40 +0900

    feat: add confirm question message

--- src/inquirer/confirm.ts
+++ src/inquirer/confirm.ts
@@ -1,8 +1,41 @@
+import pad from 'pad';
+import colors from 'colors';
+
 import { IReleaseConfig } from '../interface';
 import { inquirerConfirmQuestion } from './shared';
 
-export const askConfigConfirm = async (config: IReleaseConfig) => {
-  console.log('### release config - ', config);
+const paddingSize = 20;
+
+const log = (name: string, value: string) => {
+  console.log(pad(`${name}: `, paddingSize), colors.green(value));
+};
+
+export const askConfigConfirm = async ({
+  relBranch,
+  tagName,
+  targetPrBranchInfo,
+}: IReleaseConfig) => {
+  console.log('\n\n======================================');
+  console.log('Your Configuration');
+  console.log('======================================');
+
+  log('Release Branch', relBranch);
+  log('Tag Name', tagName);
+
+  console.log('\nCreate Pull Request & Merge Option');
+  Object.keys(targetPrBranchInfo)
+    .filter((k) => !['relBranch', 'tagName'].includes(k))
+    .map((k) => {
+      const isYN = (v: boolean) => (v ? colors.green('Y') : colors.grey('N'));
+      const obj = targetPrBranchInfo[k];
+
+      console.log(
+        pad(`  - ${k}`, paddingSize),
+        `PR(${isYN(obj.isCreate)}), Merge(${isYN(obj.isMerge)})`,
+      );
+    });
+
+  console.log('\n');
 
   return await inquirerConfirmQuestion(
     {
