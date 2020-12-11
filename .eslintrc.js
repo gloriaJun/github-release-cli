1ca7d7b2a2465b22f3dcbf16eb1734c26f15b267
gloriaJun <pureainu@gmail.com>
2020-12-11 17:20:54 +0900

    chore: modify eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,6 +25,7 @@ module.exports = {
     /**
      * basic rules
      */
+    'prettier/prettier': 'error',
     // https://eslint.org/docs/rules/no-unused-vars
     '@typescript-eslint/no-unused-vars': ['error'],
 
@@ -35,12 +36,68 @@ module.exports = {
     // '@typescript-eslint/no-use-before-define': 'warn',
 
     /**
+     * react
+     */
+
+    /**
      * import
      */
+    'import/order': [
+      'error',
+      {
+        groups: [
+          ['builtin', 'external'],
+          'internal',
+          'parent',
+          ['sibling', 'index'],
+        ],
+        'newlines-between': 'always',
+        // alphabetize: {
+        //   caseInsensitive: true,
+        //   order: 'asc',
+        // },
+      },
+    ],
+    'import/exports-last': 'error',
+    'import/extensions': [
+      'error',
+      'always',
+      {
+        js: 'never',
+        mjs: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+        vue: 'never',
+      },
+    ],
+    'import/first': 'error',
+    'import/group-exports': 'off',
+    'import/newline-after-import': 'error',
     // 'import/no-extraneous-dependencies': ['error'],
-    // 'import/no-self-import': 'error',
+    'import/no-self-import': 'error',
+    'import/no-cycle': 'error',
     // 'import/no-useless-path-segments': ['error', { noUselessIndex: true }],
     // 'import/prefer-default-export': 'off',
+    'import/no-absolute-path': 'error',
+    'import/no-internal-modules': 'error',
+    'import/no-mutable-exports': 'error',
+    'import/no-named-as-default-member': 'error',
+    'import/no-named-as-default': 'error',
+    'import/no-named-default': 'error',
+    'import/no-named-export': 'off',
+    'import/no-unresolved': ['error', { commonjs: true, caseSensitive: true }],
+    'import/no-webpack-loader-syntax': 'error',
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        // devDependencies: ["*.js", "src/**/*.test.js"],
+        devDependencies: true,
+        optionalDependencies: true,
+        peerDependencies: true,
+        bundledDependencies: true,
+      },
+    ],
   },
   // overrides: [],
   // ignorePatterns: ['dist', 'node_modules'],
